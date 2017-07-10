a6a8e662d588b735fcc72c57c294208a888b7360
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-10 09:49:44 -0400

    first commit

---
+++ MethodsPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
