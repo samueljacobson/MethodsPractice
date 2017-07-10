d041ecd738172104ac10de8741378102f878ccab
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-10 11:02:14 -0400

    added several methods

--- MethodsPractice/Program.cs
+++ MethodsPractice/Program.cs
@@ -18,14 +18,24 @@ namespace MethodsPractice
 
             Console.WriteLine("Please enter the first number to be added.");
             int numberOne = int.Parse(Console.ReadLine());
-            Console.WriteLine(("Please enter the second number to be added.");
+            Console.WriteLine(("Please enter the second number to be added."));
             int numberTwo = int.Parse(Console.ReadLine());
 
             int answer = Add(numberOne, numberTwo);
-            Console.WriteLine("The sum of your two number is " + answer);
+            Console.WriteLine("The sum of your two numbers is " + answer);
 
             //int answer = Add(5, 7);
             //Console.WriteLine(answer);
+
+            RobotWarning("Will Robinson");
+            Useless();
+
+            string myBirthMonth = "September";
+            string myFriendsBirthMonth = "December";
+            string myVehicle = Vehicle(myBirthMonth);
+            string myFriendsVehicle = Vehicle(myFriendsBirthMonth);
+
+            Console.WriteLine("My future vehicle is {0} and Jordan's future vehicle is {1}.", myVehicle, myFriendsVehicle);
         }
 
         //Method header
@@ -40,8 +50,31 @@ namespace MethodsPractice
             //The "return" keyword takes whatever value is determined by using this method and sends that value
             //back to where call method
         }
+        public static string Vehicle(string birthMonth)
+        {
+            string vehicleFortune;
+            if (birthMonth.ToUpper() == "SEPTEMBER" || birthMonth.ToUpper() == "OCTOBER")
+            {
+                vehicleFortune = "Hoverboard";
+            }
+            else
+            {
+                vehicleFortune = "SUV";
+            }
+            return vehicleFortune;
+        }
+
         //If you have a method that does not have a return value, use keyword "void"
         //When using void return type, don't need to use the keyword "return"
+
+        public static void RobotWarning(string name)
+        {
+            Console.WriteLine("Danger, " + name + "!!");
+        }
+        public static void Useless()    //no parameters, will simply print below
+        {
+            Console.WriteLine("Blah blah blah");
+        }
 
     }
 }
